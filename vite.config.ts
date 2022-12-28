d4ad3498bd93955cd0dde4e3ef4263739f4cb1ad
CaliRails <calvin98115@yahoo.com>
2022-12-27 18:02:59 -0800

    Added comment to resolve Github issue @ https://github.com/solidjs/solid-start/issues/591 and added typings/index.ts to address Typescript warnings due to missing types via module declarations

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vite";
 import vercel from "solid-start-vercel";
 
 export default defineConfig({
+  // NOTE!: Vercel project must have a custom "Build Output" set to "dist"
+  // and in addition requires this "deployment adapter" that is outlined
+  // in greater detail here: https://github.com/solidjs/solid-start/issues/591
   plugins: [solid({ adapter: vercel({ edge: true }) })],
 });
