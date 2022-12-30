58000b90a993f4774b4c766e6cc989ed8aed5901
CaliRails <calvin98115@yahoo.com>
2022-12-30 11:54:08 -0800

    Relocated `sourceMap: true` option to build config within vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
   // and in addition requires this "deployment adapter" that is outlined
   // in greater detail here: https://github.com/solidjs/solid-start/issues/591
   plugins: [solid({ adapter: vercel({ edge: true }) })],
+  build: {
+    sourcemap: true,
+  },
 });
